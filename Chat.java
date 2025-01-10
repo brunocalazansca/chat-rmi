1d0d56ec841249e973bed7da17ed9e370d2a881f
Bruno Calazans <bcalazanscarritilha@gmail.com>
2025-01-09 21:14:10 -0300

    Finalizar cliente e conexão RMI

--- Chat.java
+++ Chat.java
@@ -1,9 +1,8 @@
-import java.lang.reflect.Array;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 public interface Chat extends Remote {
     ArrayList<String> historico(String nome, String mensagem) throws RemoteException;
-    String[] mensagem (ArrayList<String> historico) throws RemoteException;
+    String mensagem (ArrayList<String> historico) throws RemoteException;
 }
--- Servidor.java
+++ Servidor.java
@@ -1,41 +1,29 @@
+import java.lang.reflect.Array;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Servidor implements Chat{
     public Servidor() {}
 
+    public static final ArrayList<String> historicoMensagem = new ArrayList<>();
+
     public ArrayList<String> historico(String nome, String mensagem) throws RemoteException {
-        ArrayList<String> mensagemRecebida = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        Chat chat = new Servidor();
+        String formatoMensagem = nome + ": " + mensagem;
 
-        while (!mensagem.equalsIgnoreCase("parar")) {
-            System.out.print("Digite o nome: ");
-            nome = scanner.nextLine();
+        historicoMensagem.add(formatoMensagem);
 
-            System.out.print("Digite o mensagem: ");
-            mensagem = scanner.nextLine();
-
-            if (!mensagem.equals("parar")) {
-                mensagemRecebida.addAll(chat.historico(nome, mensagem));
-                System.out.println("\n" + mensagemRecebida.get(mensagemRecebida.size() - 1));
-                System.out.println(mensagemRecebida);
-            }
-        }
-        return mensagemRecebida;
+        return new ArrayList<>(historicoMensagem);
     }
 
-    public String[] mensagem(ArrayList<String> historico) throws RemoteException {
-        String[] mensagem = {historico.get(historico.size() - 1)};
+    public String mensagem(ArrayList<String> historico) throws RemoteException {
+        String mensagem = historico.getLast();
         return mensagem;
     }
 
     public static void main(String[] args) {
-
         try {
             Servidor servidor = new Servidor();
             Chat stub = (Chat) UnicastRemoteObject.exportObject(servidor, 0);
@@ -45,7 +33,7 @@ public class Servidor implements Chat{
             System.err.println("Servidor está pronto...");
 
         } catch (Exception e) {
-            System.err.println("Exceção no Servidor:" + e.getMessage());
+            System.err.println("Erro no Servidor:" + e.getMessage());
         }
     }
 }
